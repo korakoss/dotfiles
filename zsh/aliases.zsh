57868f22dbdf36c9841e9392e61bfb674f96ac9c
Akos Korosi <akoskorosi@Akos-MacBook-Air.local>
2026-01-11 10:05:30 +0100

    delete commented lines, add a few custom aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# Plugin overwrites
+alias ls="eza --icons"
+alias cat="bat"
+
+
+# Python
+alias py="python3"
+alias pip="pip3"
+
+
+# Git
+alias ga="git add -u && git status"
+alias gaa="git add ."       # git add all
+alias gc="git commit -m"
+alias gcv="git commit"      # verbose/vim
+alias gs="git status"
+alias gd="git diff"
