5516161c6e2f43b4cdb861e13e23d8d1950f6f07
Ákos Kőrösi <korakos99@gmail.com>
2026-01-16 21:39:21 +0100

    add nvim binding for fuzzyfind vsplit

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,2 +1,12 @@
 -- Diagnostics
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+
+vim.keymap.set('n', '<leader>fv', function()
+  require('telescope.builtin').find_files({
+    attach_mappings = function(_, map)
+      map('i', '<CR>', require('telescope.actions').select_vertical)
+      map('n', '<CR>', require('telescope.actions').select_vertical)
+      return true
+    end
+  })
+end)
